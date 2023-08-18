8bb76815633cf3f5387efe2cd807d360aaf56f15
mion1213 <141290890+mion1213@users.noreply.github.com>
2023-08-18 16:43:58 +0000

    [Stop]user_showのリンク修正中

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   resources :books, only: [:index, :create, :show, :edit, :destroy]
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   
   get 'homes/about' => 'homes#about' , as: "about"
 end
